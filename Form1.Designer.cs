5c91bf3a99a928e0ebfc954996ecb0160766d9c3
ohegba <42731386+ohegba@users.noreply.github.com>
2018-09-01 01:51:12 -0700

    Necromancy of ancient code...

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace HotKeyed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.waitLatencyChooser = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.waitLatencyChooser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "While this window is open:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Press Win + Up to Cover the Screen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Press Win + Left to Snap \'N Expand to the Left";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Press Win + Right to Snap \'N Expand to the Right";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(37, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Press Win + Down to Minimize";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Press me to show the daemon! Nice daemon, good daemon... don\'t eat me daemon!";
+            this.notifyIcon1.BalloonTipTitle = "XPSnap Daemon";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Press me to show the daemon! Nice daemon, good daemon...";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(40, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hide Me!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // waitLatencyChooser
+            // 
+            this.waitLatencyChooser.Location = new System.Drawing.Point(244, 24);
+            this.waitLatencyChooser.Maximum = new decimal(new int[] {
+            11,
+            0,
+            0,
+            0});
+            this.waitLatencyChooser.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.waitLatencyChooser.Name = "waitLatencyChooser";
+            this.waitLatencyChooser.Size = new System.Drawing.Size(50, 20);
+            this.waitLatencyChooser.TabIndex = 6;
+            this.waitLatencyChooser.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(202, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Min. Latency to Snap";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(147, 137);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 16);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Non-Hotkey Snap Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(308, 166);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.waitLatencyChooser);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "XPSnap Daemon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.waitLatencyChooser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown waitLatencyChooser;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,480 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace HotKeyed
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        int tickCounter;
+        IntPtr lastForegroundWindow;
+
+        const int WM_HOTKEY = 0x312;
+            
+
+        int lfw_x;
+        int lfw_y;
+
+        int tickWaitLatency;
+
+        [DllImport("user32", SetLastError = true)]
+        public static extern int GetForegroundWindow();
+        GlobalHotkeys LeftSnap, RightSnap, WholeSnap, MinSnap;
+
+        public Form1()
+        {
+            tickCounter = 0;
+            InitializeComponent();
+
+            tickWaitLatency = 3;
+
+        
+
+            //  hotkey.UnregisterGlobalHotKey();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+
+            LeftSnap = new GlobalHotkeys();
+            RightSnap = new GlobalHotkeys();
+            WholeSnap = new GlobalHotkeys();
+            MinSnap = new GlobalHotkeys();
+
+            LeftSnap.UnregisterGlobalHotKey();
+            RightSnap.UnregisterGlobalHotKey();
+            WholeSnap.UnregisterGlobalHotKey();
+            MinSnap.UnregisterGlobalHotKey();
+
+            LeftSnap.RegisterGlobalHotKey((int)Keys.Left, 8, this.Handle);
+            WholeSnap.RegisterGlobalHotKey((int)Keys.Up, 8, this.Handle);
+            RightSnap.RegisterGlobalHotKey((int)Keys.Right, 8, this.Handle);
+            MinSnap.RegisterGlobalHotKey((int)Keys.Down, 8, this.Handle);
+          
+           
+        }
+
+
+        private String GetWindowTitle(IntPtr hwnd)
+        {
+           //StringBuilder sb = new StringBuilder();
+          //GetWindowText(hwnd, sb, 9999);
+           // return sb.ToString();
+            return null;
+        }
+
+
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int _Left;
+            public int _Top;
+            public int _Right;
+            public int _Bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct WINDOWINFO
+        {
+            public uint cbSize;
+            public RECT rcWindow;
+            public RECT rcClient;
+            public uint dwStyle;
+            public uint dwExStyle;
+            public uint dwWindowStatus;
+            public uint cxWindowBorders;
+            public uint cyWindowBorders;
+            public ushort atomWindowType;
+            public ushort wCreatorVersion;
+
+            public WINDOWINFO(Boolean? filler)
+                : this()
+            {
+                cbSize = (UInt32)(Marshal.SizeOf(typeof(WINDOWINFO)));
+            }
+
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [return: MarshalAs(UnmanagedType.Bool)]
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool GetWindowInfo(IntPtr hwnd, ref WINDOWINFO pwi);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private static Boolean WindowIsSizable(IntPtr hwnd)
+        {
+            WINDOWINFO info = new WINDOWINFO();
+            info.cbSize = (uint)Marshal.SizeOf(info);
+            GetWindowInfo(hwnd, ref info);
+
+            bool isSizable = (info.dwStyle & 0x00040000L) == 0x00040000L;
+
+            return isSizable;
+        }
+
+        private static int GetWindowXPos(IntPtr hwnd)
+        {
+            WINDOWINFO info = new WINDOWINFO();
+            info.cbSize = (uint)Marshal.SizeOf(info);
+            GetWindowInfo(hwnd, ref info);
+
+            int xPos = info.rcWindow._Left;
+
+            return xPos;
+        }
+
+        private static int GetWindowXWPos(IntPtr hwnd)
+        {
+            WINDOWINFO info = new WINDOWINFO();
+            info.cbSize = (uint)Marshal.SizeOf(info);
+            GetWindowInfo(hwnd, ref info);
+
+            int xPos = info.rcWindow._Right;
+
+            return xPos;
+        }
+
+
+        private static int GetWindowYPos(IntPtr hwnd)
+        {
+            WINDOWINFO info = new WINDOWINFO();
+            info.cbSize = (uint)Marshal.SizeOf(info);
+            GetWindowInfo(hwnd, ref info);
+
+            int yPos = info.rcWindow._Top;
+
+            return yPos;
+        }
+
+        private static Boolean MouseIsOnWindowCaptionBar()
+        {
+            IntPtr hwnd = new IntPtr(GetForegroundWindow());
+
+            WINDOWINFO info = new WINDOWINFO();
+            info.cbSize = (uint)Marshal.SizeOf(info);
+            GetWindowInfo(hwnd, ref info);
+
+            int cx, cy;
+            int cax, cay;
+
+            cx = (int) info.cxWindowBorders;
+            cy = (int) info.cyWindowBorders;
+
+            cax = (int)(info.rcClient._Right - info.rcClient._Left) + 3;
+            cay = (int)(info.rcClient._Bottom - info.rcClient._Top) + 3;
+
+            int wcay = info.rcWindow._Bottom - cay;
+
+            int wcay_min = GetWindowYPos(hwnd);
+
+            if (Cursor.Position.X > GetWindowXPos(hwnd) && Cursor.Position.X < GetWindowXWPos(hwnd)
+                && Cursor.Position.Y > wcay_min && Cursor.Position.Y < (wcay_min + wcay + 10))
+            {
+              //  MessageBox.Show(Cursor.Position.X.ToString() + " " + Cursor.Position.Y.ToString());
+                return true;
+            }
+
+            return false;
+        }
+
+        private static Boolean IsMouseOnLeftEdge()
+        {
+            int x = Cursor.Position.X;
+
+            return (x <= 0) ? true : false;
+        }
+
+        private static Boolean IsMouseOnRightEdge()
+        {
+           return (Cursor.Position.X >= Screen.PrimaryScreen.Bounds.Right - 5) ? true : false;
+        }
+
+        private static Boolean IsMouseOnTopEdge()
+        {
+            return (Cursor.Position.Y <= Screen.PrimaryScreen.Bounds.Top + 15) ? true : false;
+        }
+
+
+
+        private static Boolean IsWindowInLeftZone(IntPtr hwnd)
+        {
+            int x = GetWindowXPos(hwnd);
+
+            int sx = Screen.PrimaryScreen.Bounds.Width;
+
+            int zx = Screen.PrimaryScreen.Bounds.Width / 4;
+
+            if (x < zx)
+                return true;
+
+            return false;
+        }
+
+        private static Boolean IsWindowInRightZone(IntPtr hwnd)
+        {
+            int x = GetWindowXWPos(hwnd);
+
+            int sx = Screen.PrimaryScreen.WorkingArea.Width;
+
+            int zx = sx - (int)Decimal.Divide(sx, 4);
+
+            if (x > zx)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static Boolean IsWindowInTopZone(IntPtr hwnd)
+        {
+            int y = GetWindowYPos(hwnd);
+
+            int sy = Screen.PrimaryScreen.Bounds.Height;
+
+            int zy = sy / 32;
+
+            if (y < zy)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private Boolean AreSameForegroundWindowCoords()
+        {
+            IntPtr hwnd = new IntPtr(GetForegroundWindow());
+
+            int wXPos = GetWindowXPos(hwnd);
+            int wYPos = GetWindowYPos(hwnd);
+
+            if (wXPos == lfw_x && wYPos == lfw_y)
+                return true;
+
+            lfw_x = wXPos;
+            lfw_y = wYPos;
+
+            return false;
+        }
+
+        private Boolean IsSameForegroundWindow()
+        {
+            IntPtr hwnd = new IntPtr(GetForegroundWindow());
+
+            if (hwnd == lastForegroundWindow)
+                return true;
+
+            lastForegroundWindow = hwnd;
+            // MessageBox.Show("!");
+            return false;
+        }
+
+        private void SnapWindowLeft()
+        {
+            if (WindowIsSizable(new IntPtr(GetForegroundWindow())))
+            {
+                Rectangle screenie = Screen.PrimaryScreen.WorkingArea;
+                int halfWidth = screenie.Width / 2;
+
+
+                MoveWindow(new IntPtr(GetForegroundWindow()), 0, 0, halfWidth, screenie.Height, true);
+            }
+        }
+
+        private void SnapWindowRight()
+        {
+            if (WindowIsSizable(new IntPtr(GetForegroundWindow())))
+            {
+                // MessageBox.Show("RIGHT"+GetWindowTitle(new IntPtr(GetForegroundWindow())));
+                Rectangle screenie = Screen.PrimaryScreen.WorkingArea;
+                int halfWidth = screenie.Width / 2;
+                int halfHeight = screenie.Height / 2;
+
+                MoveWindow(new IntPtr(GetForegroundWindow()), halfWidth, 0, halfWidth, screenie.Height, true);
+            }
+        }
+
+        private void SnapWindowFill()
+        {
+            if (WindowIsSizable(new IntPtr(GetForegroundWindow())))
+            {
+                // MessageBox.Show("UP" + GetWindowTitle(new IntPtr(GetForegroundWindow())));
+                Rectangle screenie = Screen.PrimaryScreen.WorkingArea;
+                int halfWidth = screenie.Width / 2;
+                ShowWindow(new IntPtr(GetForegroundWindow()), 9);
+
+                MoveWindow(new IntPtr(GetForegroundWindow()), 0, 0, screenie.Width, screenie.Height, true);
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+      
+
+            switch (m.Msg)
+            {
+                              
+                case WM_HOTKEY:
+                    {
+
+                        if ((short)m.WParam == LeftSnap.HotkeyID)
+                        {
+                            //  MessageBox.Show("LEFT" + GetWindowTitle(new IntPtr(GetForegroundWindow())));
+                            SnapWindowLeft();
+                        }
+                        else if ((short)m.WParam == WholeSnap.HotkeyID)
+                        {
+                            SnapWindowFill();
+                        }
+                        else if ((short)m.WParam == RightSnap.HotkeyID)
+                        {
+                            SnapWindowRight();
+                        }
+
+                        else if ((short)m.WParam == MinSnap.HotkeyID)
+                        {
+                            if (WindowIsSizable(new IntPtr(GetForegroundWindow())))
+                            {
+                                // MessageBox.Show("RIGHT"+GetWindowTitle(new IntPtr(GetForegroundWindow())));
+                                Rectangle screenie = Screen.PrimaryScreen.Bounds;
+                                int halfWidth = screenie.Width / 2;
+                                int halfHeight = screenie.Height / 2;
+
+                                ShowWindow(new IntPtr(GetForegroundWindow()), 2);
+                            }
+                        }
+
+                        break;
+
+
+                    }
+                default:
+                    {
+                        base.WndProc(ref m);
+                        break;
+                    }
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GC.Collect();
+            this.Visible = false;
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(200);
+        }
+
+        private bool IsMouseDownAnywhere()
+        {
+            return GetAsyncKeyState(Keys.LButton) != 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IntPtr hwnd = new IntPtr(GetForegroundWindow());
+
+            if (!IsSameForegroundWindow() || !AreSameForegroundWindowCoords())
+                tickCounter = 0;
+
+            if (IsMouseOnLeftEdge() && tickCounter > tickWaitLatency && !IsMouseDownAnywhere() && MouseIsOnWindowCaptionBar())
+            {
+                SnapWindowLeft();
+                //    MessageBox.Show("SNAP");
+                tickCounter = 0;
+            }
+
+            if (IsMouseOnRightEdge() && tickCounter > tickWaitLatency && !IsMouseDownAnywhere() && MouseIsOnWindowCaptionBar())
+            {
+                SnapWindowRight();
+                //    MessageBox.Show("SNAP");
+                tickCounter = 0;
+            }
+
+            if (IsMouseOnTopEdge() && tickCounter > tickWaitLatency+2 && !IsMouseDownAnywhere() && MouseIsOnWindowCaptionBar())
+            {
+                SnapWindowFill();
+               // MessageBox.Show("SNAP");
+                tickCounter = 0;
+            }
+
+            if (IsWindowInLeftZone(hwnd) && IsMouseDownAnywhere() && IsMouseOnLeftEdge())
+            {
+                //   Console.Beep();
+                tickCounter++;
+            }
+            else if (IsWindowInRightZone(hwnd) && IsMouseDownAnywhere() && IsMouseOnRightEdge())
+            {
+                //   Console.Beep();
+                tickCounter++;
+            }
+            else if (IsWindowInTopZone(hwnd) && IsMouseDownAnywhere() && IsMouseOnTopEdge())
+            {
+                tickCounter++;
+            }
+            else
+            {
+                tickCounter = 0;
+            }
+
+
+
+        }
+
+        private void waitLatencyChooser_ValueChanged(object sender, EventArgs e)
+        {
+            tickWaitLatency = (int)waitLatencyChooser.Value;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MouseIsOnWindowCaptionBar();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            button1_Click(null, null);
+        }
+
+    }
+}
